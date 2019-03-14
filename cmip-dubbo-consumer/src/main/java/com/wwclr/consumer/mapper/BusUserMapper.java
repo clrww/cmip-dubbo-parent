82b4ca21eac3d230a92e166b93989ad7a7d08cd7
wangwu <wangwu@ngarihealth.com>
2019-03-14 10:59:03 +0800

    框架搭建

--- cmip-dubbo-consumer/src/main/java/com/wwclr/consumer/mapper/BusUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wwclr.consumer.mapper;
-
-import com.wwclr.consumer.pojo.BusUser;
-import com.wwclr.consumer.utils.MyMapper;
-
-public interface BusUserMapper extends MyMapper<BusUser> {
-}
