ff0508d691c5ae04df167ba9668ef0d467e7ce77
wwclr <17710634541@163.com>
2019-03-17 21:02:22 +0800

    代码修改

--- cmip-dubbo-provide/src/main/java/com/wwclr/ProviderApplication.java
+++ cmip-dubbo-provide/src/main/java/com/wwclr/ProviderApplication.java
@@ -2,10 +2,15 @@ package com.wwclr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by xebest on 2018/1/4.
  */
+//@ImportResource("classpath:application.properties")      //导入xml配置
+
+
+@ImportResource("classpath:spring-hibernate.xml")      //导入xml配置
 @SpringBootApplication
 public class ProviderApplication {
     public static void main(String[] args){
