151a42c6dadc2414fbf4755e53eb3747d7e0d1cc
wangwu <wangwu@ngarihealth.com>
2019-03-19 18:00:43 +0800

    框架搭建

--- cmip-dubbo-consumer/src/main/java/com/wwclr/consumer/controller/StudentController.java
+++
@@ -1,7 +0,0 @@
-package com.wwclr.consumer.controller;
-
-/**
- * Created by wwclr on 2019/3/7.
- */
-public class StudentController {
-}
--- cmip-dubbo-consumer/src/main/java/com/wwclr/consumer/controller/ThymeleafController.java
+++ cmip-dubbo-consumer/src/main/java/com/wwclr/consumer/controller/ThymeleafController.java
@@ -1,6 +1,12 @@
 package com.wwclr.consumer.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSONObject;
+import com.wwclr.api.bean.BusUserBean;
+import com.wwclr.api.service.BusUserInterface;
 import com.wwclr.consumer.entity.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +21,13 @@ import java.util.List;
 @Controller
 @RequestMapping("/th")
 public class ThymeleafController {
+        private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
-        @RequestMapping("/index")
+        @Reference
+        BusUserInterface busUserInterface;
+
+
+        @RequestMapping("/indexceshi")
         public String index(ModelMap modelMap){
                 modelMap.addAttribute("name1","wangwu");
                 return "thymeleaf/ceshi";
@@ -41,5 +52,30 @@ public class ThymeleafController {
         public String postform(Student student){
                 System.out.println(student.getName());
                 return "redirect:/th/student";
+        }
+
+        @RequestMapping("/index22")
+        public String index22(BusUserBean busUserBean){
+                busUserBean.setCrateTime(new Date());
+                busUserBean.setUserId("987543121212121");
+                busUserBean.setAge(26);
+                busUserBean.setUserName("wangwu");
+                busUserBean.setPhone(10086);
+                LOGGER.info("UserController  saveUser ={}", JSONObject.toJSON(busUserBean));
+                busUserInterface.saveUser(busUserBean);
+                return "thymeleaf/index22";
+        }
+
+        @RequestMapping("/index")
+        public String index11(ModelMap modelMap){
+                List<BusUserBean> userList=busUserInterface.findAllUser();
+
+//                ModelMap modelMap =new ModelMap();
+                Student student=new Student("123","supermanager",18,new Date());
+//                modelMap.addAttribute("student",student);
+//                modelMap.addAttribute("name","wangwu");
+                modelMap.addAttribute("userList",userList);
+                LOGGER.info("UserController  saveUser ={}", JSONObject.toJSON(student));
+                return "thymeleaf/index";
         }
 }
