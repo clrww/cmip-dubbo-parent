af3bb3eb2521c2cb6bc48bae6acc71eba3e88772
wangwu <wangwu@ngarihealth.com>
2020-07-07 16:18:44 +0800

    项目搭建

--- cmip-dubbo-consumer/src/main/java/com/wwclr/consumer/controller/DubboController.java
+++ cmip-dubbo-consumer/src/main/java/com/wwclr/consumer/controller/DubboController.java
@@ -6,9 +6,7 @@ import com.wwclr.api.service.DubboService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by xebest on 2018/1/4.
- */
+
 @RestController
 @RequestMapping("/dubbo")
 public class DubboController {
--- cmip-dubbo-provide/src/main/java/com/wwclr/provide/service/impl/DubboServiceImpl.java
+++ cmip-dubbo-provide/src/main/java/com/wwclr/provide/service/impl/DubboServiceImpl.java
@@ -3,9 +3,7 @@ package com.wwclr.provide.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.wwclr.api.service.DubboService;
 
-/**
- * Created by xebest on 2018/1/4.
- */
+
 @Service
 public class DubboServiceImpl implements DubboService {
     @Override
